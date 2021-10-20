8ae8247e9e8cfec9ceb543eff349bf0f13b9efa7
igor.sobral <igor.sobral@pagtel.com.br>
2021-10-19 23:20:45 -0300

    chore: finish application adding websocket with socketio

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
 import "dotenv/config";
 import express from "express";
+import http from "http"
+import { Server } from 'socket.io';
+import cors from "cors";
 
 import { router } from "./routes";
 
-const app = express()
+const app = express();
+app.use(cors())
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+    cors: {
+        origin: "*"
+    }
+});
+
+
+io.on("connection", socket => {
+    console.log(`Usuário conectado no socket  ${socket.id}`)
+});
 
 app.use(express.json());
 
@@ -17,9 +34,7 @@ app.get('/github' , (req, res) => {
 app.get("/signin/callback", (req, res) => {
     const { code } = req.query;
     
-    return res.json(code)
-})
+    return res.json(code);
+});
 
-app.listen(4000, () => {
-    console.log('Server is running on PORT 4000')
-})
+export { serverHttp, io };
